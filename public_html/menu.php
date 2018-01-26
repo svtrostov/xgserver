4d91f509d68ae2ed7da0cfe8d4846da5e663323e
Stanislav V. Tretyakov <svtrostov@yandex.ru>
2018-01-26 11:55:26 +0300

    Add files via upload

---
+++ public_html/menu.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>XG Server static page</title>
+	<style>
+	
+	* {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+	}
+	
+	body {
+		font: 50px arial, helvetica, sans-serif;
+		background: #fed;
+	}
+	
+	li a {
+		display: block;
+		width: 250px;
+		padding: 25px;
+		margin: 25px auto;
+		background: white;
+		box-shadow: 0 0 5px 0 rgba(63,31,0,.5);
+		border-radius: 10px;
+		text-align: center;
+		text-decoration: none;
+		-webkit-transition: .2s;
+		transition: .2s;
+	}
+	
+	li a:hover {
+		-webkit-transform: rotate(-10deg) scale(2);
+		transform: rotate(-10deg) scale(2);
+	}
+
+	
+	</style>
+</head>
+
+<body>
+
+	<ul>
+		<li><a href="#">Welcome</a></li>
+		<li><a href="#">To</a></li>
+		<li><a href="#">XGServer</a></li>
+		<li><a href="#">Static</a></li>
+		<li><a href="#">Page!</a></li>
+	</ul>
+
+
+</body>
+
+</html>
